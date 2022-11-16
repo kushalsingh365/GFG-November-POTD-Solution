009bfd6bf73d8f06a45b2df9f35b3f15ceb3599a
Kushal Singh <81277166+kushalsingh365@users.noreply.github.com>
2022-11-16 10:48:37 +0530

    Sum of Beauty of All Substrings

---
+++ 16_Nov_22.cpp
@@ -0,0 +1,49 @@
+class Solution {
+  public:
+    int beautySum(string s) {
+        // Your code goes here
+         int n=s.size();
+
+        int ans=0;
+
+        for(int i=0;i<n;i++)
+
+        {
+
+              vector<int>cnt(26,0);
+
+              for(int j=i;j<n;j++)
+
+              {
+
+                  cnt[s[j]-'a']++;
+
+              int maxi=INT_MIN,mini=INT_MAX;
+
+              for(int k=0;k<26;k++)
+
+              {
+
+                  if(cnt[k]>0)
+
+                  mini=min(mini,cnt[k]);
+
+                  maxi=max(maxi,cnt[k]);
+
+              }
+
+             // cout<<mini<<" "<<maxi<<"\n";
+
+              ans+=(maxi-mini);
+
+              }
+
+              
+
+            
+
+        }
+
+        return ans;
+    }
+};
